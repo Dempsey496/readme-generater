154b5ce8eed71bdc63f0905a83aa26de51f43624
Dempsey FP <dempseyfinley@gmail.com>
2020-11-02 18:33:09 -0500

    test

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Fuck trump");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer
+.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+]);
+
